1139c9386ec0f87816d90993dd6b64431f2ccf6b
nvdat16 <nguyenvandat160105@gmail.com>
2025-09-27 23:06:41 +0700

    inital commit

--- datasets/coco.py
+++ datasets/coco.py
@@ -159,9 +159,10 @@ def build(image_set, args):
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "train", root / "train" / "_annotations.coco.json"),
+        "val": (root / "test", root / "test" / "_annotations.coco.json"),
     }
+
 
     img_folder, ann_file = PATHS[image_set]
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks,
