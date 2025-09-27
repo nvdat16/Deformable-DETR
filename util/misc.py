b14f2336fa0ebe885e934a13b66d91d6d943214d
nvdat16 <nguyenvandat160105@gmail.com>
2025-09-27 23:42:51 +0700

    Inital commit

--- util/misc.py
+++ util/misc.py
@@ -29,7 +29,16 @@ from torch import Tensor
 import torchvision
 if float(torchvision.__version__[:3]) < 0.5:
     import math
-    from torchvision.ops.misc import _NewEmptyTensorOp
+    class _NewEmptyTensorOp(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, x, new_shape):
+            ctx.shape = x.shape
+            return x.new_empty(new_shape)
+
+        @staticmethod
+        def backward(ctx, grad):
+            shape = ctx.shape
+            return _NewEmptyTensorOp.apply(grad, shape), None
     def _check_size_scale_factor(dim, size, scale_factor):
         # type: (int, Optional[List[int]], Optional[float]) -> None
         if size is None and scale_factor is None:
